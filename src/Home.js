4b270833bcc2359d61c1c6244c72f19e924d3084
devhiko <aydin96tr@gmail.com>
2022-04-02 00:42:20 +0300

    come to custom hooks

--- src/Home.js
+++ src/Home.js
@@ -4,20 +4,35 @@ import BlogList from "./BlogList";
 const Home = () => {
 
   const [blogs, SetBlogs] = useState(null);
+  const [isPending, setPending] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
-    fetch('http://localhost:8000/blogs')
-      .then(res => res.json())
-      .then(data => SetBlogs(data))
-      .catch(err => console.log(err))
+    setTimeout(() => {
+      fetch('http://localhost:8000/blogss')
+        .then(res => {
+          if (!res.ok) {
+            throw Error('Error while fetching data !')
+          }
+          return res.json();
+        })
+        .then(data => {
+          SetBlogs(data);
+          setPending(false);
+          setError(null);
+        })
+        .catch(err => {
+          setPending(false);
+          setError(err.message);
+        });
+    }, 1000);
   }, []);
 
   return (
     <div className="home">
-      {blogs && <BlogList
-        blogs={blogs}
-        title="All Blogs"
-      />}
+      {error && <div className="error">{error}</div>}
+      {isPending && <div>Fetching data...</div>}
+      {blogs && <BlogList blogs={blogs} title="All Blogs" />}
     </div>
   );
 }
